283ecbef55ff0ca82c08957a53ccd85879761076
ToufouMaster <owen.kala@gmx.fr>
2023-08-16 15:21:41 +0200

    Removed useless code

--- src/main/java/toufoumaster/btwaila/tooltips/block/TrommelTooltip.java
+++ src/main/java/toufoumaster/btwaila/tooltips/block/TrommelTooltip.java
@@ -29,13 +29,5 @@ public class TrommelTooltip implements IBTWailaCustomBlockTooltip {
 
         ProgressBarOptions options = new ProgressBarOptions().setText("Progress: ");
         guiBlockOverlay.drawProgressBarWithText((int) trommel.getCookProgressPercent(100), 100, options, 32);
-
-        int value = (int) trommel.getCookProgressPercent(15000);
-        int max = 15000;
-        ProgressBarOptions options2 = new ProgressBarOptions().setText("Lava: "+value+"/"+max+"mb").setBoxWidth(150).setValues(false).setPercentage(false);
-        TextureOptions bgOpt = new TextureOptions().setBlockId(Block.fluidLavaStill.id).setColor(Colors.GRAY);
-        TextureOptions fgOpt = new TextureOptions().setBlockId(Block.fluidLavaStill.id).setColor(Colors.LIGHT_GRAY);
-        options2.setBackgroundOptions(bgOpt).setForegroundOptions(fgOpt);
-        guiBlockOverlay.drawProgressBarTextureWithText(value, max, options2, 32);
     }
 }
