1e49569b99b4cd80849ad96ff3e09dbe21930d59
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-10 08:26:09 -0500

    Revert "Only send tileEntityPacket when needed"
    This reverts commit ebaff3e661a97f9ee5892c3a41d49a81716f8126.

--- src/main/java/toufoumaster/btwaila/IPlayerPacketData.java
+++
@@ -1,9 +0,0 @@
-package toufoumaster.btwaila;
-
-import com.mojang.nbt.CompoundTag;
-import net.minecraft.core.net.packet.Packet140TileEntityData;
-
-public interface IPlayerPacketData {
-    Packet140TileEntityData getPreviousTileEntityData();
-    void setPreviousTileEntityData(Packet140TileEntityData packet);
-}
