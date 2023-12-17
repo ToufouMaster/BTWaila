9953f4a9afadf366cc963942549771c01ea7aefa
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-12-17 08:39:19 -0600

    Updated flag tooltip

--- src/main/java/toufoumaster/btwaila/tooltips/block/FlagTooltip.java
+++ src/main/java/toufoumaster/btwaila/tooltips/block/FlagTooltip.java
@@ -25,19 +25,20 @@ public class FlagTooltip implements IBTWailaCustomBlockTooltip {
         ItemStack color1 = flag.items[0];
         ItemStack color2 = flag.items[1];
         ItemStack color3 = flag.items[2];
-        renderStringAndStack(guiBlockOverlay,"Color 1: "+ ((color1 != null) ? stringTranslate.translateKey(color1.getItemTranslateKey()) : "No Item"), 0, color1);
-        renderStringAndStack(guiBlockOverlay,"Color 2: "+ ((color2 != null) ? stringTranslate.translateKey(color2.getItemTranslateKey()) : "No Item"), 0, color2);
-        renderStringAndStack(guiBlockOverlay,"Color 3: "+ ((color3 != null) ? stringTranslate.translateKey(color3.getItemTranslateKey()) : "No Item"), 0, color3);
+        renderStringAndStack(guiBlockOverlay,stringTranslate.translateKey("btwaila.tooltip.flag.color").replace("{d}", "1") + "    " +  ((color1 != null) ? stringTranslate.translateKey(color1.getItemTranslateKey()) : stringTranslate.translateKey("btwaila.noitem")), 0, color1);
+        renderStringAndStack(guiBlockOverlay,stringTranslate.translateKey("btwaila.tooltip.flag.color").replace("{d}", "2") + "    " +  ((color2 != null) ? stringTranslate.translateKey(color2.getItemTranslateKey()) : stringTranslate.translateKey("btwaila.noitem")), 0, color2);
+        renderStringAndStack(guiBlockOverlay,stringTranslate.translateKey("btwaila.tooltip.flag.color").replace("{d}", "3") + "    " +  ((color3 != null) ? stringTranslate.translateKey(color3.getItemTranslateKey()) : stringTranslate.translateKey("btwaila.noitem")), 0, color3);
     }
-    private void renderStringAndStack(GuiBlockOverlay guiBlockOverlay, String s, int offX, ItemStack stack){
+    protected void renderStringAndStack(GuiBlockOverlay guiBlockOverlay, String s, int offX, ItemStack stack){
+        I18n stringTranslate = I18n.getInstance();
         if (stack != null){
             int y = guiBlockOverlay.getOffY() - 1;
-            int x = guiBlockOverlay.getPosX() + guiBlockOverlay.getGame().fontRenderer.getStringWidth(s) + 33;
+            int x = guiBlockOverlay.getPosX() + 16 + guiBlockOverlay.minecraft.fontRenderer.getStringWidth(stringTranslate.translateKey("btwaila.tooltip.flag.color").replace("{d}", "1") + "    ");
             y -= 3;
-            guiBlockOverlay.itemRender.renderItemIntoGUI(guiBlockOverlay.getGame().fontRenderer, guiBlockOverlay.getGame().renderEngine, stack, x, y, 1.0F);
-            guiBlockOverlay.itemRender.renderItemOverlayIntoGUI(guiBlockOverlay.getGame().fontRenderer, guiBlockOverlay.getGame().renderEngine, stack, x, y, 1.0F);
+            GuiBlockOverlay.itemRender.renderItemIntoGUI(guiBlockOverlay.getGame().fontRenderer, guiBlockOverlay.getGame().renderEngine, stack, x, y, 1.0F);
+            GuiBlockOverlay.itemRender.renderItemOverlayIntoGUI(guiBlockOverlay.getGame().fontRenderer, guiBlockOverlay.getGame().renderEngine, stack, x, y, 1.0F);
             GL11.glDisable(GL11.GL_LIGHTING);
-        }
+        };
         guiBlockOverlay.drawStringWithShadow(s, offX);
         guiBlockOverlay.addOffY(4);
     }
