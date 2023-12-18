3e96e635c972cffb6735a37c9111617c00d1d54c
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-12-18 16:01:15 -0600

    Update JarToolTip.java

--- src/main/java/toufoumaster/btwaila/tooltips/block/JarToolTip.java
+++ src/main/java/toufoumaster/btwaila/tooltips/block/JarToolTip.java
@@ -1,10 +1,12 @@
 package toufoumaster.btwaila.tooltips.block;
 
+import net.minecraft.client.render.Lighting;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.entity.TileEntityFlowerJar;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import toufoumaster.btwaila.demo.DemoEntry;
+import toufoumaster.btwaila.demo.DemoManager;
 import toufoumaster.btwaila.gui.components.AdvancedInfoComponent;
 import toufoumaster.btwaila.tooltips.TileTooltip;
 
@@ -33,15 +35,19 @@ public class JarToolTip extends TileTooltip<TileEntityFlowerJar> {
         int y = advancedInfoComponent.getOffY() + 1;
         advancedInfoComponent.drawStringWithShadow(text, 0);
 
+        Lighting.enableInventoryLight();
         if (flower != null){
             int x = advancedInfoComponent.getPosX() + advancedInfoComponent.getGame().fontRenderer.getStringWidth(text) + 2;
             y -= 4;
             itemRender.renderItemIntoGUI(advancedInfoComponent.getGame().fontRenderer, advancedInfoComponent.getGame().renderEngine, flower, x, y, 1.0F);
             itemRender.renderItemOverlayIntoGUI(advancedInfoComponent.getGame().fontRenderer, advancedInfoComponent.getGame().renderEngine, flower, x, y, 1.0F);
         }
+        Lighting.disable();
     }
     @Override
     public DemoEntry tooltipDemo(Random random){
-        return new DemoEntry(Block.jarGlass, 0, new TileEntityFlowerJar(), new ItemStack[]{Item.jar.getDefaultStack()});
+        TileEntityFlowerJar flowerJar = new TileEntityFlowerJar();
+        flowerJar.flowerInPot = DemoManager.randomStack(random).getItem().id;
+        return new DemoEntry(Block.jarGlass, 0, flowerJar, new ItemStack[]{Item.jar.getDefaultStack()});
     }
 }
